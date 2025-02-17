becc38ec9e2bac3e6e76c6a933f3991e9b00a5d3
Reuben Frankel <rfrankel@matatika.com>
2025-02-17 17:01:19 +0000

    Flatten top-level properties and convert to snake-case

--- mapper_fivetran/mapper.py
+++ mapper_fivetran/mapper.py
@@ -4,13 +4,60 @@ from __future__ import annotations
 
 import typing as t
 
+import humps
 import singer_sdk.typing as th
 from singer_sdk import _singerlib as singer
-from singer_sdk.mapper import PluginMapper
+from singer_sdk.helpers._flattening import FlatteningOptions
+from singer_sdk.helpers._util import utc_now
+from singer_sdk.mapper import DefaultStreamMap, PluginMapper
 from singer_sdk.mapper_base import InlineMapper
+from typing_extensions import override
 
 if t.TYPE_CHECKING:
     from pathlib import PurePath
+
+
+class FivetranStreamMap(DefaultStreamMap):
+    """Fivetran default stream map."""
+
+    @override
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(
+            *args,
+            flattening_options=FlatteningOptions(max_level=1, separator="_"),
+        )
+
+        self._transform_schema(self.transformed_schema)
+        self._transform_key_properties(self.transformed_key_properties)
+
+    @override
+    def transform(self, record):
+        record: dict[str] = super().transform(record)
+
+        for name in record.copy():
+            record[self._transform_name(name)] = record.pop(name)
+
+        return record
+
+    @override
+    def get_filter_result(self, record):
+        return True
+
+    def _transform_schema(self, schema: dict[str]) -> None:
+        properties: dict[str] = schema["properties"]
+
+        for name in properties.copy():
+            properties[self._transform_name(name)] = properties.pop(name)
+
+    @staticmethod
+    def _transform_name(name: str) -> str:
+        name = humps.decamelize(name)
+        return name.replace(".", "_")
+
+    @classmethod
+    def _transform_key_properties(cls, key_properties: list[str]) -> None:
+        for i, name in enumerate(key_properties):
+            key_properties[i] = cls._transform_name(name)
 
 
 class FivetranMapper(InlineMapper):
@@ -51,7 +98,11 @@ class FivetranMapper(InlineMapper):
             validate_config=validate_config,
         )
 
-        self.mapper = PluginMapper(plugin_config=dict(self.config), logger=self.logger)
+        self.mapper = PluginMapper(
+            plugin_config=self.config,
+            logger=self.logger,
+        )
+        self.mapper.default_mapper_type = FivetranStreamMap
 
     def map_schema_message(self, message_dict: dict) -> t.Iterable[singer.Message]:
         """Map a schema message to zero or more new messages.
@@ -59,7 +110,21 @@ class FivetranMapper(InlineMapper):
         Args:
             message_dict: A SCHEMA message JSON dictionary.
         """
-        yield singer.SchemaMessage.from_dict(message_dict)
+        self._assert_line_requires(message_dict, requires={"stream", "schema"})
+
+        stream_id: str = message_dict["stream"]
+        self.mapper.register_raw_stream_schema(
+            stream_id,
+            message_dict["schema"],
+            message_dict.get("key_properties", []),
+        )
+        for stream_map in self.mapper.stream_maps[stream_id]:
+            yield singer.SchemaMessage(
+                stream_map.stream_alias,
+                stream_map.transformed_schema,
+                stream_map.transformed_key_properties,
+                message_dict.get("bookmark_keys", []),
+            )
 
     def map_record_message(
         self,
@@ -70,7 +135,18 @@ class FivetranMapper(InlineMapper):
         Args:
             message_dict: A RECORD message JSON dictionary.
         """
-        yield singer.RecordMessage.from_dict(message_dict)
+        self._assert_line_requires(message_dict, requires={"stream", "record"})
+
+        stream_id: str = message_dict["stream"]
+        for stream_map in self.mapper.stream_maps[stream_id]:
+            mapped_record = stream_map.transform(message_dict["record"])
+            if mapped_record is not None:
+                yield singer.RecordMessage(
+                    stream=stream_map.stream_alias,
+                    record=mapped_record,
+                    version=message_dict.get("version"),
+                    time_extracted=utc_now(),
+                )
 
     def map_state_message(self, message_dict: dict) -> t.Iterable[singer.Message]:
         """Map a state message to zero or more new messages.
